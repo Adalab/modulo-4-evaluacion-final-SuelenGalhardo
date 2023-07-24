085a933022716a4ffa06c379f4069146567e1902
SuelenGalhardo <suelenranucci@gmail.com>
2023-07-24 11:31:40 +0200

    added recetas and  endpoints id

---
+++ recetas.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM recetas_db.recetas
+LIMIT 0, 1000
+
+-- Date: 2023-07-24 10:27
+*/
+INSERT INTO `` (`id`,`nombre`,`ingredientes`,`instruciones`) VALUES (1,'Guaspacho','tomate, pepino ','bater todo en la licuadora');
+INSERT INTO `` (`id`,`nombre`,`ingredientes`,`instruciones`) VALUES (2,'crepes caseras','Huevos, leche, harina','batimos todos los ingredientes en un bol, una vez integrada y sin grumos , debemos añadir la mezcla en una sartén caliente con un poco de aceite, ');
--- src/index.js
+++ src/index.js
@@ -5,12 +5,10 @@
 
 // Imports
 
-const express = require("express");
-const cors = require("cors");
-const mysql = require("mysql2/promise");
-require('dotenv').config()
-
-
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 // Arracar el servidor
 
@@ -19,29 +17,21 @@ const server = express();
 // Configuración del servidor
 
 server.use(cors());
-server.use(express.json({limit: "25mb"}));
+server.use(express.json({ limit: '25mb' }));
 server.set('view engine', 'ejs');
-
-
 
 // Conexion a la base de datos
 
 async function getConnection() {
-  const connection = await mysql.createConnection(
-    {
-      host: process.env.DB_HOST || "localhost",
-      user: process.env.DB_USER || "root",
-      password: process.env.DB_PASS,  // <-- Pon aquí tu contraseña o en el fichero /.env en la carpeta raíz
-      database: process.env.DB_NAME || "Clase",
-    }
-  );
-
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME || 'recetas_db',
+  });
   connection.connect();
-
   return connection;
 }
-
-
 
 // Poner a escuchar el servidor
 
@@ -50,37 +40,36 @@ server.listen(port, () => {
   console.log(`Ya se ha arrancado nuestro servidor: http://localhost:${port}/`);
 });
 
-
-
 // Endpoints
 
 // GET /api/items
-
-server.get("/api/items", async (req, res) => {
-
-  const selectProducts = "SELECT * FROM products";
-
+server.get('/recetas', async (req, res) => {
+  const selectRE = 'SELECT * FROM recetas';
   const conn = await getConnection();
-
-  const [results] = await conn.query(selectProducts);
-
-  console.log(results);
-
+  const [result] = await conn.query(selectRE);
+  console.log(result);
   conn.end();
-
-  res.json(results);
+  res.json({
+    info: { count: result.length },
+    results: result,
+  });
 });
 
-
-
-// GET /details
-
-server.get("/details", async (req, res) => {
-
-  res.render('details', {})
+//Obtener una receta por su ID (GET /recetas/:id)
+server.get('/recetas/:id', async (req, res) => {
+  const RecetaId = req.params.id;
+  const selectRE = 'select * from recetas where id = ?';
+  const conn = await getConnection();
+  const [result] = await conn.query(selectRE, [RecetaId]);
+  conn.end();
+  console.log(result);
+  res.json({
+    results: result,
+  });
 });
 
+//Crear una nueva receta (POST /recetas):
 
-// Serv estáticos
+//Actualizar una receta existente (PUT /recetas/:id):
 
-server.use(express.static("./src/public_html"));
+//Eliminar una receta (DELETE /recetas/:id)
