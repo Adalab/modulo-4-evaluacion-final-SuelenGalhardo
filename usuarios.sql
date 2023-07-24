de0b89db168a579b773a53dc239d9987532d5655
SuelenGalhardo <suelenranucci@gmail.com>
2023-07-24 13:27:01 +0200

    adde new database usuarios

---
+++ usuarios.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM `usuarios_db"`.usuarios
+LIMIT 0, 1000
+
+-- Date: 2023-07-24 13:26
+*/
+INSERT INTO `` (`idusuarios`,`email`,`nombre`,`password`) VALUES (1,'suelengalhardo@gmail.com','Suelen','832415');
+INSERT INTO `` (`idusuarios`,`email`,`nombre`,`password`) VALUES (2,'juanFerraz@gmail.com','Juan','852415');
